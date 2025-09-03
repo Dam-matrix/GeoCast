99022c886201c26b740464e4000bb20549cfc65f
Dam-matrix <damexxxr@gmail.com>
2025-09-03 17:36:30 +0100

    Update server.py

--- server.py
+++ server.py
@@ -85,6 +85,4 @@ def view_weather():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-#https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=581629c78280fad5305ccabdad3c5351
+    
